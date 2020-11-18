4dd3ba51ec1e36d1c0f6d607c046aff77e9270c7
Arsen <arsen.avetisyan.2000@mail.ru>
2020-11-18 17:47:42 +0300

    Added post-method and check work

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express');
-const express = require('express');
 const bodyParser = require('body-parser');
 const _ = require('lodash');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.post('/', (req, res) => {
+    if (!req.body) return res.sendStatus(400);
+    console.log(req.body);
+    res.send('It is Work')
+});
+
+
+app.listen(8080);
